f03c34719b9fbcd512a471ff4d2919c8b21ff549
Berkan <ujnaa@student.kit.edu>
2025-07-10 18:13:26 +0200

    sec

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-import farming.model.VegetableType;
-
-public class Main {
-    public static void main(String[] args) {
-        VegetableType type = VegetableType.TOMATO;
-        System.out.println(type.getGrowTime());
-    }
-}
